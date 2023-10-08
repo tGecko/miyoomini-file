cad2a4f9e12481cfe4506c919f3f9d5362402bc3
tGecko <tGecko@proton.me>
2023-10-08 19:54:15 +0200

    add output folder generation

--- mk.sh
+++ mk.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 autoreconf -f -i
 make distclean
+rm -rf output/
 ./configure --prefix "/mnt/SDCARD/.tmp_update/" --disable-silent-rules --host arm-linux-gnueabihf --target arm-linux-gnueabihf
 make -j4
+mkdir -p output/bin output/lib output/share/misc/
+cp src/.libs/file output/bin/
+cp src/.libs/libmagic.so* output/lib
+cp magic/magic.mgc output/share/misc/
